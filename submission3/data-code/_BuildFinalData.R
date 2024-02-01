dbd7f049cd9fd3b8f47ab9eb2ac2e597927586c7
Kpollard <kendall.pollard@emory.edu>
2024-02-01 10:13:16 -0600

    work

--- submission3/data-code/_BuildFinalData.R
+++ submission3/data-code/_BuildFinalData.R
@@ -15,10 +15,12 @@ pacman::p_load(tidyverse, ggplot2, dplyr, lubridate, stringr, readxl, data.table
 
 # Call individual scripts -------------------------------------------------
 
-source("data-code/1_Plan_Data.R")
-source("data-code/2_Plan_Characteristics.R")
-source("data-code/3_Service_Areas.R")
-source("data-code/4_Penetration_Files.R")
+
+source("submission3/data-code/1_Plan_Data.R")
+source("submission3/data-code/2_Plan_Characteristics.R")
+source("submission3/data-code/3_Service_Areas.R")
+source("submission3/data-code/4_Penetration_Files.R")
+
 source("data-code/5_Star_Ratings.R")
 source("data-code/6_Risk_Rebates.R")
 source("data-code/7_MA_Benchmark.R")
